7d387684ae13dd7e443ab130332ca4b31ef67f27
Amal Tapalov <amaltapalov@gmail.com>
2019-10-07 11:37:11 +0200

    Styled component using CSS module

--- src/components/container.js
+++ src/components/container.js
@@ -4,12 +4,10 @@ import containerStyles from "./container.module.css"
 export default ({ children }) => (
   <>
     <div className={containerStyles.container}>{children}</div>
-    <div>
-      <img
-        className={containerStyles.myImage}
-        src="https://source.unsplash.com/random/400x200"
-        alt=""
-      />
-    </div>
+    <img
+      className={containerStyles.myImage}
+      src="https://source.unsplash.com/random/400x200"
+      alt=""
+    />
   </>
 )
