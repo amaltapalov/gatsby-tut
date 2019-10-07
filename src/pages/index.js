bac7124d079e5dc252c45e5bc3fd12c3e3ba1fd6
Amal Tapalov <amaltapalov@gmail.com>
2019-10-07 11:57:11 +0200

    Refactor mynav component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
+import MyNav from "../components/mynav"
 import Header from "../components/header"
+
 export default () => (
   <div style={{ color: `purple` }}>
-    <div>
-      <Link to="/contact/">Contact </Link>
-    </div>
-    <div>
-      <Link to="/about-css-modules/">About CSS modules </Link>
-    </div>
+    <MyNav />
     <Header headerText="Hello Gatsby!" />
     <p>What a world</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
